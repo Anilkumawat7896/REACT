ea0fa6990bd6f51db115d6bacee5376d8c28112d
Anil <anilkumawat7896@gmail.com>
2023-10-20 22:58:59 +0530

    inception 01

---
+++ inception/01_Inception/script.js
@@ -0,0 +1,7 @@
+const heading = document.createElement("h1");
+
+heading.innerHTML = "Hellow World From JavaScript";
+
+const root = document.getElementById("root");
+
+root.appendChild(heading);
---
+++ inception/Introducing React/App.js
@@ -0,0 +1,54 @@
+// here the object if for the attributes you will give to your tag  syntex ---: attribute_name : "attribute_value"
+
+/* const heading = React.createElement(
+  "h1",
+  {
+    style: {
+      color: "red",
+      backgroundColor: "blue",
+      "font-size": "60px",
+    },
+    title: "Intro message",
+  },
+  "Hellow World from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); */
+
+//  how to make nested elements
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading1 = React.createElement(
+  "h1",
+  { className: "heading" },
+  "This is how React Operate!"
+);
+const heading2 = React.createElement(
+  "h2",
+  { className: "heading" },
+  "This is the Second Heading!"
+);
+
+// const childDiv = React.createElement("div", { id: "child" }, heading1);
+
+/* 
+
+if you want to give siblings then do this
+react will thow a error like you need a key something like that
+
+*/
+
+const childDiv = React.createElement("div", { id: "child" }, [
+  heading1,
+  heading2,
+]);
+
+const parentDiv = React.createElement("div", { id: "parent" }, childDiv);
+
+root.render(parentDiv);
+
+
+//  we left at 1.6.15 in inception in react
