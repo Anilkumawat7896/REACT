71500d5e9b8c85e5eb6ddb5fc9fa3b4ce77f3a3c
Anil <anilkumawat7896@gmail.com>
2023-10-21 12:35:13 +0530

    inception is compelete

--- inception/Introducing React/App.js
+++ inception/Introducing React/App.js
@@ -19,18 +19,18 @@ root.render(heading); */
 
 //  how to make nested elements
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading1 = React.createElement(
-  "h1",
-  { className: "heading" },
-  "This is how React Operate!"
-);
-const heading2 = React.createElement(
-  "h2",
-  { className: "heading" },
-  "This is the Second Heading!"
-);
+// const heading1 = React.createElement(
+//   "h1",
+//   { className: "heading" },
+//   "This is how React Operate!"
+// );
+// const heading2 = React.createElement(
+//   "h2",
+//   { className: "heading" },
+//   "This is the Second Heading!"
+// );
 
 // const childDiv = React.createElement("div", { id: "child" }, heading1);
 
@@ -41,14 +41,45 @@ react will thow a error like you need a key something like that
 
 */
 
-const childDiv = React.createElement("div", { id: "child" }, [
+/* const childDiv = React.createElement("div", { id: "child" }, [
   heading1,
   heading2,
 ]);
 
 const parentDiv = React.createElement("div", { id: "parent" }, childDiv);
 
-root.render(parentDiv);
-
+root.render(parentDiv); */
 
 //  we left at 1.6.15 in inception in react
+
+// nested child components
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parentDiv = React.createElement("div", { className: "parent" }, [
+  React.createElement("div", { className: "child1" }, [
+    React.createElement("h1", {}, "H1 from child one div"),
+    React.createElement("h2", {}, "H1 from child one div"),
+    React.createElement("hr"),
+  ]),
+  React.createElement("div", { className: "child2" }, [
+    React.createElement("h1", {}, "H1 from child two div"),
+    React.createElement("h2", {}, "H1 from child two div"),
+  ]),
+]);
+
+root.render(parentDiv);
+
+/* 
+
+siblings can not be created like this because react will overwrite this 
+
+    const randomHeading = React.createElement(
+      "h1",
+      { className: "Heading" },
+      "This is a random Heading"
+    );
+
+    root.render(randomHeading);
+
+*/
